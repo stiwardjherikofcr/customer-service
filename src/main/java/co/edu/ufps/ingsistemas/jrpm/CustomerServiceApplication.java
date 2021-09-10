a8c4c9a473ed3f89cf10ac5a384c93b7438d82c4
STIWARD JHERIKOF CARRILLO RAMIREZ <stiwardjherikofcr@ufps.edu.co>
2021-09-10 00:17:17 -0500

    bug fixes

--- src/main/java/co/edu/ufps/ingsistemas/jrpm/CustomerServiceApplication.java
+++ src/main/java/co/edu/ufps/ingsistemas/jrpm/CustomerServiceApplication.java
@@ -1,4 +1,4 @@
-package co.edu.ufps.ingsistemas.jrpm.customerservice;
+package co.edu.ufps.ingsistemas.jrpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
